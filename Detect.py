bd28415502ae6b5a108a014511a2b7cec9a74e85
阿福 <1306701599@qq.com>
2024-12-29 16:04:36 +0800

    初始化
    1.模型训练
    2.模型验证
    3.图像分割并保存
    4.调用摄像头
    5.颜色检测

---
+++ Detect.py
@@ -0,0 +1,8 @@
+﻿from ultralytics import YOLO
+
+if __name__ == '__main__':
+    Model = YOLO(r"C:\Yolo\YoloTrain\Module\1024\11x550\detect\train\weights\best.pt")
+    for i in range(20):
+        str1 = "C:/Yolo/YoloTrain/Pic/"
+        str2 = ".png"
+        Model.predict(str1 + str(i + 1) + str2, save=True, conf=0.05)
---
+++ Val.py
@@ -0,0 +1,12 @@
+﻿import cv2
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+# 加载模型
+    model = YOLO(r"C:\Yolo\YoloTrain\v8x440\detect\train\weights\best.pt")  # 加载自定义模型
+
+    # 验证模型
+    metrics = model.val()  # 不需要额外参数，数据集和设置会被记住
+    print(metrics.box.map)  # mAP50-95
+    print(metrics.box.map50)  # mAP50
+    print(metrics.box.map75)  # mAP75
